46149e64fcb04be08ef7ed777648d2d6fb50d32b
Bao <nguyeenngiabao@gmail.com>
2026-06-08 20:46:11 +0700

    Change SEARCH_TAG from Hatsune Miku to Satono Diamond

--- bot.py
+++ bot.py
@@ -7,7 +7,7 @@ from playwright.async_api import async_playwright
 
 TOKEN = os.getenv("DISCORD_TOKEN")
 CHANNEL_ID = os.getenv("DISCORD_CHANNEL_ID")
-SEARCH_TAG = os.getenv("SEARCH_TAG", "Hatsune+Miku")
+SEARCH_TAG = os.getenv("SEARCH_TAG", "Satono+Diamond")
 
 CACHE_FILE = "posted.txt"
 
